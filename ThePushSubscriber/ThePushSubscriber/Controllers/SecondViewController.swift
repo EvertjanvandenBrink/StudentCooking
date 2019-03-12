7ffe2dcafb7e76dc1b310f1d9c560e8e0e5cc6df
evertjanvandenbrink <EJ.vandenBrink1@student.han.nl>
2019-03-12 20:01:07 +0100

    Tried to get api items

--- ThePushSubscriber/ThePushSubscriber/Controllers/SecondViewController.swift
+++ ThePushSubscriber/ThePushSubscriber/Controllers/SecondViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import Alamofire
 
 class SecondViewController: UIViewController {
+    var API_URL: String = "https://newsapi.org/v2"
+    var API_KEY: String = "f03288f1a1cb47c9bf386e40cf4a867c"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        fetchAllHNItems()
+        fetchTopHeadlines()
     }
+    
+    func fetchTopHeadlines() {
+        Alamofire.request("https://newsapi.org/v2/top-headlines", parameters: ["country": "nl", "apiKey": "f03288f1a1cb47c9bf386e40cf4a867c"])
+            .response { data in
+                print(request)
+                print(data)
+        }
+        //        Alamofire.request(getData(url: "top-headlines?country=nl")).responseData { (resData) -> Void in
+//            print(resData.result.value!)
+//        let strOutput = String(data : resData.result.value!, encoding : String.Encoding.utf8)
+//            print(strOutput)
+//        }
+    }
+    
+    // With Alamofire
+    func fetchAllHNItems() {
+        guard let url = URL(string: getData(url: "top-headlines?country=nl")) else {
+            return
+        }
+        Alamofire.request(url, method: .get)
+//                          parameters: ["include_docs": "true"]
+            .validate()
+            .responseJSON { response in
+                guard response.result.isSuccess else {
+                    print("Error while fetching hackernews items: \(response.result.error)")
+                    return
+                }
 
-
+                guard let value = response.result.value as? [String: Any],
+                    let rows = value["rows"] as? [[String: Any]] else {
+                        print("Malformed data received from fetchAllHNItems service")
+                        return
+                }
+               
+        }
+    }
+    
+    func getData(url: String) -> String {
+        return "\(API_URL)/\(url)&apiKey=\(API_KEY)";
+    }
 }
-
