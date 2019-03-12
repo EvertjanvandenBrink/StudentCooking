565eaa577e22d34e882fc6f5b930da403db2464e
evertjanvandenbrink <EJ.vandenBrink1@student.han.nl>
2019-03-12 20:26:44 +0100

    Added functionallity to get items from the hn api

--- ThePushSubscriber/ThePushSubscriber/Controllers/SecondViewController.swift
+++ ThePushSubscriber/ThePushSubscriber/Controllers/SecondViewController.swift
@@ -16,43 +16,19 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        fetchAllHNItems()
-        fetchTopHeadlines()
+        fetchAllItems()
+//        fetchTopHeadlines()
     }
     
     func fetchTopHeadlines() {
-        Alamofire.request("https://newsapi.org/v2/top-headlines", parameters: ["country": "nl", "apiKey": "f03288f1a1cb47c9bf386e40cf4a867c"])
-            .response { data in
-                print(request)
-                print(data)
+        Alamofire.request(getData(url: "top-headlines?country=nl")).responseJSON { (resData) -> Void in
+            print(resData.result.value!)
         }
-        //        Alamofire.request(getData(url: "top-headlines?country=nl")).responseData { (resData) -> Void in
-//            print(resData.result.value!)
-//        let strOutput = String(data : resData.result.value!, encoding : String.Encoding.utf8)
-//            print(strOutput)
-//        }
     }
-    
-    // With Alamofire
-    func fetchAllHNItems() {
-        guard let url = URL(string: getData(url: "top-headlines?country=nl")) else {
-            return
-        }
-        Alamofire.request(url, method: .get)
-//                          parameters: ["include_docs": "true"]
-            .validate()
-            .responseJSON { response in
-                guard response.result.isSuccess else {
-                    print("Error while fetching hackernews items: \(response.result.error)")
-                    return
-                }
-
-                guard let value = response.result.value as? [String: Any],
-                    let rows = value["rows"] as? [[String: Any]] else {
-                        print("Malformed data received from fetchAllHNItems service")
-                        return
-                }
-               
+   
+    func fetchAllItems() {
+        Alamofire.request(getData(url: "everything?sources=nl")).responseJSON { (resData) -> Void in
+            print(resData.result.value!)
         }
     }
     
