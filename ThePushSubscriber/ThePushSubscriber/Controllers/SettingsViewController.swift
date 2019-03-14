b9321d3b6edfaed0005acf84aff62075157dacb1
evertjanvandenbrink <EJ.vandenBrink1@student.han.nl>
2019-03-14 12:55:19 +0100

    Created controllers

---
+++ ThePushSubscriber/ThePushSubscriber/Controllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  ThePushSubscriber
+//
+//  Created by Evertjan van den Brink on 12/03/2019.
+//  Copyright © 2019 Evertjan van den Brink. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
