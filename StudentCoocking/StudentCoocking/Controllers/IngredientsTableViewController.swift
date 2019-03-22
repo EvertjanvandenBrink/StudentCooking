bbb86b39b56628edcc82ea08864981d87a0142d7
evertjanvandenbrink <EJ.vandenBrink1@student.han.nl>
2019-03-22 14:40:31 +0100

    Added ingredients page with controller

---
+++ StudentCoocking/StudentCoocking/Controllers/IngredientsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IngredientsTableViewController.swift
+//  StudentCoocking
+//
+//  Created by Evertjan van den Brink on 22/03/2019.
+//  Copyright © 2019 Evertjan van den Brink. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
