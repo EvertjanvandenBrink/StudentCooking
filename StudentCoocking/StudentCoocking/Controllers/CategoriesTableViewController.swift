bc3ba9115b6a8000aaa21face040f85a4511d425
evertjanvandenbrink <EJ.vandenBrink1@student.han.nl>
2019-04-05 15:12:42 +0200

    Displayed categories with images

---
+++ StudentCoocking/StudentCoocking/Controllers/CategoriesTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CategoriesTableViewController.swift
+//  StudentCoocking
+//
+//  Created by Evertjan van den Brink on 05/04/2019.
+//  Copyright © 2019 Evertjan van den Brink. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+        var categories = [Category]()
+        
+        func completionFetchAllCategories(categories: [Category]?, error: Error?) {
+            if let categories = categories {
+                self.updateUI(with: categories)
+            }
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            TheMealDBService.shared.fetchAllCategories(completionHandler: completionFetchAllCategories)
+        }
+        
+        func updateUI(with categories: [Category]) {
+            DispatchQueue.main.async {
+                self.categories = categories
+                self.tableView.reloadData()
+            }
+        }
+        
+        override func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return categories.count
+        }
+        
+        override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 100
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "categoryIdentifier", for: indexPath)
+            configure(cell: cell, forItemAt: indexPath)
+            return cell
+        }
+        
+        func configure(cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+            let category = categories[indexPath.row]
+            
+            cell.textLabel?.text = category.strCategory
+            if let url = URL(string: category.strCategoryThumb) {
+                TheMealDBService.shared.fetchImage(url: url) { (image) in
+                    guard let image = image else { return }
+                    DispatchQueue.main.async {
+                        if let currentIndexPath = self.tableView.indexPath(for: cell),
+                            currentIndexPath != indexPath {
+                            return
+                        }
+                        cell.imageView?.image = image
+                    }
+                }
+            }
+        }
+}
