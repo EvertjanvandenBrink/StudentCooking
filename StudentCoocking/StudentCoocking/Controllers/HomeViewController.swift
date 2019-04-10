c9563e33dd35bf04566a99090ad418d41380df69
evertjanvandenbrink <EJ.vandenBrink1@student.han.nl>
2019-04-10 11:34:35 +0200

    Created some tests for subscriptionsHelperClass

--- StudentCoocking/StudentCoocking/Controllers/HomeViewController.swift
+++ StudentCoocking/StudentCoocking/Controllers/HomeViewController.swift
@@ -12,9 +12,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
